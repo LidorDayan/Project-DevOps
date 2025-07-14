80866ce27eaae2ab309564a8ef2c9d13f5721e21
LidorDayan <lidordayan1999@gmail.com>
2025-07-14 21:24:34 +0300

    Update adhoc.sh

--- ansible/adhoc.sh
+++ ansible/adhoc.sh
@@ -4,5 +4,5 @@ echo "🔍 Checking Docker version on worker..."
 ansible worker -i inventory.ini -m shell -a "docker --version" --become --extra-vars "ansible_become_pass=$1"
 
 echo "🔍 Checking Kubernetes services on worker..."
-ansible worker -i inventory.ini -m shell -a "KUBECONFIG=/home/lidorwo/k3s.yaml kubectl get svc" --become --extra-vars "ansible_become_pass=$1"
+ansible worker -i inventory.ini -m shell -a "KUBECONFIG=/home/[worker user name]/k3s.yaml kubectl get svc" --become --extra-vars "ansible_become_pass=$1"
 
