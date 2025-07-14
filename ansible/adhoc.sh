ac2969a6b386a7ae3abf9d3161ea5318d12ce972
LidorDayan <lidordayan1999@gmail.com>
2025-07-14 14:13:49 +0300

    add ansible folder

---
+++ ansible/adhoc.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "🔍 Checking Docker version on worker..."
+ansible worker -i inventory.ini -m shell -a "docker --version" --become --extra-vars "ansible_become_pass=$1"
+
+echo "🔍 Checking Kubernetes services on worker..."
+ansible worker -i inventory.ini -m shell -a "KUBECONFIG=/home/lidorw/k3s.yaml kubectl get svc" --become --extra-vars "ansible_become_pass=$1"
+
